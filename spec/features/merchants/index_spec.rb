e372f92bab9f47a869bbccfc0e222b02b60a6a0e
Tay <tay.james.d@gmail.com>
2019-06-24 19:18:17 -0600

    Setting up the database

--- spec/features/merchants/index_spec.rb
+++ spec/features/merchants/index_spec.rb
@@ -1,12 +1,15 @@
 require "rails_helper"
 
-RSpec.describe "merchants index page", type: :feature do
-  it "user can see all merchants" do
-    merchant_1 = Merchant.create()
+RSpec.describe "Merchant Index"  do
+  describe "As a Visitor" do
+    it "I see all the name of each merchant in the stystem" do
+      merchant_1 = Merchant.create(name: "Valentino")
 
-    visit "/merchants"
-    expect(page).to have_content(merchant_1)#what attributes does our merchant have?
-    expect(page).to have_content()
+      visit "/merchants"
+
+      save_and_open_page
+      
+    end
   end
 end
 #is this named index_spec bc we're creating the index page?
